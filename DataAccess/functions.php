79aacf7c2b0ed112dd5792f15c55ec6256d0e2f9
techbabette <57938200+RosefireRF@users.noreply.github.com>
2023-03-09 07:24:57 +0100

    Create new listing function

--- DataAccess/functions.php
+++ DataAccess/functions.php
@@ -1,6 +1,7 @@
 <?php
 function createNewUser($email, $password, $name, $lastName){
     include ("connection.php");
+
     $crypted = md5($password);
     $statement = "INSERT INTO users (email, password, name, lastName) 
                   VALUES (?, ?, ?, ?)";
@@ -13,4 +14,25 @@ function createNewUser($email, $password, $name, $lastName){
     $prepSt->execute(); 
 }
 
+function createNewListing($user, $borough, $building_type, $name, $description, $address, $size){
+    include ("connection.php");
+
+    $statement = "INSERT INTO listings (user_id, borough_id, building_type_id, listing_name, description, address, size) 
+                  VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+    $prepSt = $conn->prepare($statement);
+    $prepSt->bindParam(1, $user);
+    $prepSt->bindParam(2, $borough);
+    $prepSt->bindParam(3, $building_type);
+    $prepSt->bindParam(4, $name);
+    $prepSt->bindParam(5, $description);
+    $prepSt->bindParam(6, $address);
+    $prepSt->bindParam(7, $size);
+
+    $prepSt->execute(); 
+
+    $last_id = $conn->lastInsertedId();
+    
+    return $last_id;
+}
 ?>
