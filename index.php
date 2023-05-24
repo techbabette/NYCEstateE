037597e9e4ea71b6336df3f37046dca7ecb16ef2
techbabette <marinakrsticrf@gmail.com>
2023-05-24 07:30:53 +0200

    Different pages now have different head titles

--- index.php
+++ index.php
@@ -1,26 +1,43 @@
+<?php
+//Decide what file to include here, include later
+$page = null;
+$fileToInclude;
+$title;
+if(isset($_GET["page"])){
+  $page = $_GET["page"];
+}
+if(!$page){
+  $title = "Index";
+  $fileToInclude = "./views/pages/index.html";
+}
+else{
+    $success = file_exists("./views/pages/".$page);
+    if(!$success){
+      header("Location: index.php");
+    }
+    else{
+      $fileToInclude = ("./views/pages/".$page);
+    }
+    $title = ucfirst(explode(".",$page)[0]);
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
-<?php include("./views/fixed/head.html")?>
+
+<?php 
+      include("./views/fixed/head.php");
+      echoHead($title);
+?>
+
 <body>
+
     <!--Navbar-->
     <?php include("./views/fixed/nav.html")?>
       <!--Content-->
-      
       <?php
-      $page = null;
-      if(isset($_GET["page"])){
-        $page = $_GET["page"];
-      }
-      if(!$page){
-        include("./views/pages/index.html");
-      }
-      else{
-          $success = include("./views/pages/".$page);
-          if(!$success){
-            header("Location: index.php");
-          }
-      }
+        include($fileToInclude);
       ?>
+      
       <?php include("./views/fixed/footer.html")?>
       <?php include("./views/fixed/endOfBodyScripts.html")?>
 </body>
