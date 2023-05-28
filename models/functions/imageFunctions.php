d16d2b1c7c8a4b98d0d1a094cdcb99186e0bc745
techbabette <marinakrsticrf@gmail.com>
2023-05-28 08:41:00 +0200

    Position image on bottom of background

--- models/functions/imageFunctions.php
+++ models/functions/imageFunctions.php
@@ -4,7 +4,7 @@ function saveAdjustedPhotoToDisk($image, $targetFile, $maxW, $maxH){
     $file_name = $image['tmp_name'];
     list($width, $height, $type, $attr) = getimagesize( $file_name );
     /*/If the width is greater than the height, calculate scaling 
-    ratio based on width, else calculated based on height */
+    ratio based on width, else calculate based on height */
     if($width > $height){
         $ratio = $maxW / $width;
     }
@@ -30,7 +30,7 @@ function saveAdjustedPhotoToDisk($image, $targetFile, $maxW, $maxH){
     $bg = imagecolorallocate ($newDst, 31, 39, 27);
     imagefilledrectangle($newDst, 0, 0, $maxW, $maxH, $bg);
     //Merge the resized image onto the background image, positioning it to the center in case of smaller width
-    imagecopymerge($newDst, $dst, ($maxW - $new_width) / 2, 0, 0, 0, $new_width, $new_height, 100);
+    imagecopymerge($newDst, $dst, ($maxW - $new_width) / 2, $maxH - $new_height, 0, 0, $new_width, $new_height, 100);
     //Save the new image as a jpeg
     $result = imagejpeg($newDst, $targetFile);
     //Destory the source image
